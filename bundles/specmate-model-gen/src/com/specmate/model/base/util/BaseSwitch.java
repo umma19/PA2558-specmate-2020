a06af4088e83642476779e26eee4ba97459bfdc7
Reza Arvandian <reza.arvandian@gmail.com>
2020-10-03 13:37:09 +0200

    Too many lines in comment

--- bundles/specmate-model-gen/src/com/specmate/model/base/util/BaseSwitch.java
+++ bundles/specmate-model-gen/src/com/specmate/model/base/util/BaseSwitch.java
@@ -292,10 +292,7 @@ public class BaseSwitch<T> extends Switch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>Folder</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
+	 * This implementation returns null; returning a non-null result will terminate the switch.
 	 * @param object the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>Folder</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
