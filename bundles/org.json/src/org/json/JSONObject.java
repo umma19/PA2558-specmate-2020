5a02230d162f16a6798186d4f0ad3e83b26e56a8
maryam-rezaei <62109941+maryam-rezaei@users.noreply.github.com>
2020-10-04 23:24:39 +0200

    Change the use of double and integer Issue #58

--- bundles/org.json/src/org/json/JSONObject.java
+++ bundles/org.json/src/org/json/JSONObject.java
@@ -1075,7 +1075,7 @@ public class JSONObject {
      *             If the key is null or if the number is invalid.
      */
     public JSONObject put(String key, double value) throws JSONException {
-        this.put(key, new Double(value));
+        this.put(key, Double.valueOf(value));
         return this;
     }
 
@@ -1091,7 +1091,7 @@ public class JSONObject {
      *             If the key is null.
      */
     public JSONObject put(String key, int value) throws JSONException {
-        this.put(key, new Integer (value));
+        this.put(key, Integer.valueOf(value));
         return this;
     }
 
